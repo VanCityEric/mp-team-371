90735428ef9829a79d2ef73eb8b5ee1b28be1503
CarlinChow <carlin_chow@sfu.ca>
2023-11-28 11:46:39 -0800

    some stuff

--- web_server.py
+++ web_server.py
@@ -19,23 +19,26 @@ while True:
     data = connectionSocket.recv(1024).decode('utf-8')
     method = data.split(' ')[0]
     route = data.split(' ')[1]
-    header = data.split("\r\n\r\n")[0].split("\r\n")[1:]
+    print(method)
+    print(route)
+    headerRaw = data.split("\r\n\r\n")[0].split("\r\n")[1:]
     print(data + "\n")
-    print(header)
-    # headerDict = {}
-    # for heading in header:
-    #     heading.split()
+    header = {}
+    for heading in headerRaw:
+        key = heading.split(":")[0]
+        value = heading.split(":")[1]
+        header[key] = value
     
-    f = open("test.html", "r")
-    http = "HTTP/1.1 200 OK\n" + "Content-Type: text/html\n"+"\n"+f.read()
-
-    # if method == 'GET':
-    #     if route == ("/test.html"):
-    #         f = open("test.html", "r")
-    #         http = "HTTP/1.1 200 OK\n" + "Content-Type: text/html\n"+"\n"+f.read()
-    #     else:
-    #         http = "HTTP/1.1 404 Not Found\n\n404 Not Found"
-    
+    if method == "GET":
+        if route == "/test.html":
+            if "If-Modified-Since" in header:
+                print("here")
+                http = "HTTP/1.1 304 Not Modified\r\n"
+            else: 
+                f = open("test.html", "r")
+                http = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\n"+"\n"+f.read()
+    else:
+        http = "HTTP/1.1 404 Not Found\r\n404 Not Found"
     
 
     # do stuff
@@ -43,4 +46,3 @@ while True:
     connectionSocket.send(http.encode())
     connectionSocket.shutdown(SHUT_WR)
     connectionSocket.close()
-connectionSocket.close()
